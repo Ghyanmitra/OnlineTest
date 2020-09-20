aee00a9950d2eded669fcaea89275f6ee8197443
ghyanmitrajiblapnor <ghyanmitrajiblapnor@gmail.com>
2020-09-20 16:24:13 +0530

    initial commit

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if(Auth::user()->role=='Admin'){
+
+            return $next($request);
+
+        }elseif(Auth::user()->role=='Guest'){
+
+            return redirect('/home');
+
+        }else{
+
+            return redirect('/');
+
+        }
+    }
+}
